a5e88b0a3528797e99c4e6cd3c8c819071cc4231
manshi413 <manshiyadav413@gmail.com>
2025-04-12 16:07:16 +0530

    Second

--- app.js
+++ app.js
@@ -16,12 +16,9 @@ const PORT = process.env.PORT || 5000; // Default port is 5000 if not provided i
 const MONGO_URI = process.env.MONGO_URI; // MongoDB connection string from .env
 
 // Connect to MongoDB
-mongoose.connect(MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-    .then(() => console.log("Connected to MongoDB"))
-    .catch((err) => console.error("Error connecting to MongoDB:", err));
+mongoose.connect(MONGO_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Error connecting to MongoDB:", err));
 
 // Define routes
 app.get('/', (req, res) => {
